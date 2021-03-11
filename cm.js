c2be2087068d901ea9be4e2f24f152e69c6b28f1
triplelog <robmwilc@gmail.com>
2021-03-11 14:11:01 -0500

    nm

--- cm.js
+++ cm.js
@@ -1,9 +1,19 @@
 import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
+import {EditorView, keymap} from "@codemirror/view"
+import {defaultTabBinding} from "@codemirror/commands"
 import {javascript} from "@codemirror/lang-javascript"
+
+const doc = `if (true) {
+  console.log("okay")
+} else {
+  console.log("oh no")
+}
+`
 
 let editor = new EditorView({
   state: EditorState.create({
-    extensions: [basicSetup, javascript()]
+    doc,
+    extensions: [basicSetup, keymap.of([defaultTabBinding]), javascript()]
   }),
   parent: document.body
 })
