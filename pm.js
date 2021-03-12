9f937692e96cca3f4f7036bde5c35796ba196d53
triplelog <robmwilc@gmail.com>
2021-03-12 10:43:35 -0500

    nm

--- pm.js
+++ pm.js
@@ -98,7 +98,7 @@ function inputUp(evt){
 	console.log(sel);
 	tt.setSelection(sel);
 	console.log(tt);
-	tt.deleteSelction();
+	tt.deleteSelection();
 	console.log(tt);
 	myView.dispatch(tt);
 	console.log(myView);
