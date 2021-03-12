ebcbaf6d2745b770c4fb25895f6f41410551f48f
triplelog <robmwilc@gmail.com>
2021-03-12 08:06:02 -0500

    nm

--- pm.js
+++ pm.js
@@ -1,5 +1,5 @@
 import {EditorState} from "prosemirror-state"
-import {EditorView} from "prosemirror-view"
+import {EditorView} from "@prosemirror-view"
 import {Schema, DOMParser} from "prosemirror-model"
 import {schema} from "prosemirror-schema-basic"
 import {addListNodes} from "prosemirror-schema-list"
