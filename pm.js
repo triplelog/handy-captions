fdee48e8c87bb3ed419ac9fb6e653934d8b7af81
triplelog <robmwilc@gmail.com>
2021-03-12 10:41:33 -0500

    nm

--- pm.js
+++ pm.js
@@ -4,7 +4,7 @@ import {Schema, DOMParser} from "prosemirror-model"
 import {schema} from "prosemirror-schema-basic"
 import {addListNodes} from "prosemirror-schema-list"
 import {exampleSetup} from "prosemirror-example-setup"
-import {Selection, Transaction} from "prosemirror-state"
+import {TextSelection, Selection, Transaction} from "prosemirror-state"
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
