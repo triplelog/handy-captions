1329d25100e87fb4966cd5631b42325870524d82
triplelog <robmwilc@gmail.com>
2021-03-12 14:18:11 -0500

    nm

--- pm.js
+++ pm.js
@@ -44,13 +44,14 @@ let syncPlugin = new Plugin({
   filterTransaction: (t,s) => {
     var myId = s.doc.attrs.id;
     console.log(t.getMeta('k'));
+    console.log(myId);
 
   	if (myId == 0 && !t.getMeta('k')){
   		t.doc = myViews[1].state.doc;
   		myViews[1].dispatch(t);
   		return false;
   	}
-  	else if (!t.getMeta('k')) {
+  	else if (myId == 1 && !t.getMeta('k')) {
   		t.doc = myViews[0].state.doc;
   		t.setMeta('k',true);
   		myViews[0].dispatch(t);
