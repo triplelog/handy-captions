593fd43f3169d57895ad9f14e84e331609666138
triplelog <robmwilc@gmail.com>
2021-03-13 08:28:58 -0500

    nm

--- pm.js
+++ pm.js
@@ -324,7 +324,7 @@ export function chgTab(from,to) {
 	myViews[to].updateState(myViews[to].state);
 	var ttt = myViews[to].state.tr;
 	ttt.setMeta('k',true);
-	var rPos1 = myViews[to].state.docs.resolve(selectedText[to].start);
+	var rPos1 = myViews[to].state.doc.resolve(selectedText[to].start);
 	var s = new Selection(rPos1,rPos1);
 	ttt.setSelection(s);
 	ttt.scrollIntoView();
