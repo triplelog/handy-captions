c08cfd3fb72ff4d5f99b14fec2d1579bbe729892
triplelog <robmwilc@gmail.com>
2021-03-18 12:42:00 -0400

    nm

--- static/js/outlines.js
+++ static/js/outlines.js
@@ -709,11 +709,11 @@ function outline(pd,margin,direction){
 			newPath.style.fill = "url(#box-grad-"+i+"-"+id+")";
 		
 			if (i%2 == 0){
-				newPath.setAttribute('stroke-width','1.0');
+				newPath.setAttribute('stroke-width','0.0');
 				newPath.setAttribute('stroke',"green");
 			}
 			else {
-				newPath.setAttribute('stroke-width','1.0');
+				newPath.setAttribute('stroke-width','0.0');
 				newPath.setAttribute('stroke',"green");
 			}
 		
