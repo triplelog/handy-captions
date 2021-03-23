5ed900352b38d674cc515bfc36c4bee64b9406aa
triplelog <robmwilc@gmail.com>
2021-03-23 14:44:12 -0400

    nm

--- static/js/follow.js
+++ static/js/follow.js
@@ -755,7 +755,7 @@ function outline(pd,margin,direction){
 	newPath.setAttribute('d',outPaths[0]);
 	newPath.style.fill = "none";
 
-	newPath.setAttribute('stroke-width','6');
+	newPath.setAttribute('stroke-width','3');
 	if (direction == 'in'){
 		newPath.setAttribute('stroke',"red");
 	}
