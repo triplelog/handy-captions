c8d2ae7ddfbbdc23e5da837acf7714c203ba4717
triplelog <robmwilc@gmail.com>
2021-03-28 11:25:51 -0400

    nm

--- static/js/magiceye.js
+++ static/js/magiceye.js
@@ -183,13 +183,13 @@
 	  for (y = 0; y < height; y++) {
 	  	for (x = (width - 1); x >= 0; x--) {
 	  		if (y > 400){
-				pixels[(y * width * 4) + (x - 0) * 4] = 0;
+				pixels[(y * width * 4) + (x - 0) * 4] = pixels[(y * width * 4) + (x * 4) + 3];
 				pixels[(y * width * 4) + (x - 0) * 4 + 1] = 0;
-				pixels[(y * width * 4) + (x - 0) * 4 + 2] = pixels[(y * width * 4) + (x * 4) + 3];
+				pixels[(y * width * 4) + (x - 0) * 4 + 2] = 0;
 				//pixels[(y * width * 4) + (x - 0) * 4 + 3] = 255;
 			}
 			else if (y > 200){
-				pixels[(y * width * 4) + (x - 0) * 4] = pixels[(y * width * 4) + (x * 4) + 3];
+				pixels[(y * width * 4) + (x - 0) * 4] = 0;
 				pixels[(y * width * 4) + (x - 0) * 4 + 1] = 0;
 				pixels[(y * width * 4) + (x - 0) * 4 + 2] = pixels[(y * width * 4) + (x * 4) + 3];
 				//pixels[(y * width * 4) + (x - 0) * 4 + 3] = 255;
@@ -205,11 +205,12 @@
 	  }
 	  for (var i=0;i<fronts.length;i++){
 	  	if (fronts[i][0] > 400){
-	  		pixels[(fronts[i][0] * width * 4) + (fronts[i][1]) * 4] = 30;
-	  		pixels[(fronts[i][0] * width * 4) + (fronts[i][2]) * 4] = 10;
+	  		pixels[(fronts[i][0] * width * 4) + (fronts[i][1]) * 4 + 2] = 30;
+	  		pixels[(fronts[i][0] * width * 4) + (fronts[i][2]) * 4 + 2] = 10;
 	  	}
 	  	else if (fronts[i][0] > 200){
-	  		
+	  		pixels[(fronts[i][0] * width * 4) + (fronts[i][1]) * 4 ] = 30;
+	  		pixels[(fronts[i][0] * width * 4) + (fronts[i][2]) * 4 ] = 10;
 	  	}
 	  	else {
 	  		pixels[(fronts[i][0] * width * 4) + (fronts[i][1]) * 4 + 2] = 30;
