32b941e9451bb0c1b4e4510d0ba12b36b7bbd384
triplelog <robmwilc@gmail.com>
2021-03-28 11:12:40 -0400

    nm

--- static/js/magiceye.js
+++ static/js/magiceye.js
@@ -191,10 +191,8 @@
 	  	}
 	  }
 	  for (var i=0;i<fronts.length;i++){
-	  	var l = fronts[i][2]-fronts[i][1];
-	  	for (var ii=fronts[i][1];ii<fronts[i][2]+1;ii++){
-	  		pixels[(fronts[i][0] * width * 4) + (ii) * 4] += (fronts[i][2]-ii)*5/l;
-	  	}
+	  		pixels[(fronts[i][0] * width * 4) + (fronts[i][1]) * 4] += 1;
+	  		pixels[(fronts[i][0] * width * 4) + (fronts[i][2]) * 4] += 1;
 	  }
 	  
 	  for (y = 0; y < height; y++) {
