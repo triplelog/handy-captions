dc35c23c65a9b466457b88a06388f99c92ee19da
triplelog <robmwilc@gmail.com>
2021-03-28 13:53:06 -0400

    nm

--- static/js/magiceye.js
+++ static/js/magiceye.js
@@ -225,7 +225,7 @@
             }
             
           }
-          pixels[pixelOffset + 0] += x / 800;
+          pixels[pixelOffset + 0] += x * 100 / 800;
           if (pixels[pixelOffset + 0] > 255){pixels[pixelOffset + 0] = 255;}
         }
        
