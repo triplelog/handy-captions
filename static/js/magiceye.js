d572d78339968137b6d0b5dae6dfd0e6efa98ce8
triplelog <robmwilc@gmail.com>
2021-03-28 13:25:46 -0400

    nm

--- static/js/magiceye.js
+++ static/js/magiceye.js
@@ -199,11 +199,11 @@
             	pF += fs[chain[x][i]];
             }
             rgba = opts.colors[Math.floor(Math.random() * numColors)];
-            if (pF > 1){
-            	rgba[0] = 128;
-            }
-            //rgba[0] = 50 * pF / (chain[x].length);
-            //if (rgba[0] > 50){rgba[0] = 50;}
+            //if (pF > 1){
+            //	rgba[0] = 128;
+            //}
+            rgba[0] = 50 * pF / (chain[x].length);
+            if (rgba[0] > 50){rgba[0] = 50;}
             for (i = 0; i < 4; i++) {
               pixels[pixelOffset + i] = rgba[i];
             }
