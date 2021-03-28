1f0e3099982aa714d95e8badba08c4456f5db739
triplelog <robmwilc@gmail.com>
2021-03-28 13:43:01 -0400

    nm

--- static/js/magiceye.js
+++ static/js/magiceye.js
@@ -208,8 +208,8 @@
             rgba = opts.colors[Math.floor(Math.random() * numColors)];
             
             rgba[0] = ax * 255 / 800;
-            rgba[1] = Math.floor(Math.random() * 255);
-            rgba[2] = Math.floor(Math.random() * 255);
+            rgba[1] = 0;// Math.floor(Math.random() * 255);
+            rgba[2] = 0;//Math.floor(Math.random() * 255);
             rgba[3] = 255;
             
             if (rgba[0] > 50){rgba[0] = 50;}
