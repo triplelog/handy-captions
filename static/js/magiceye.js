cddf6cd7a2d03ccd24812ee41236f2d1581271b9
triplelog <robmwilc@gmail.com>
2021-03-28 10:01:33 -0400

    nm

--- static/js/magiceye.js
+++ static/js/magiceye.js
@@ -19,7 +19,7 @@
         depthMapper: new MagicEye.DepthMapper(),
         imageType: 'png',
         colors: [
-          [255, 255, 255, 255],
+          [255, 0, 0, 255],
           [0, 0, 0, 255]
         ]
       };
