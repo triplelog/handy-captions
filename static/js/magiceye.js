7df95c14e95838cd13710f693fed156ec27abe75
triplelog <robmwilc@gmail.com>
2021-03-28 10:18:28 -0400

    nm

--- static/js/magiceye.js
+++ static/js/magiceye.js
@@ -19,11 +19,7 @@
         depthMapper: new MagicEye.DepthMapper(),
         imageType: 'png',
         colors: [
-          [255, 0, 0, 255],
-          [0, 0, 0, 255]
-        ],
-        colors2: [
-          [0, 0, 255, 255],
+          [0, 0, 0, 0],
           [0, 0, 0, 255]
         ]
       };
@@ -59,19 +55,12 @@
           opts.colors[i] = this.helpers.hexToRGBa(opts.colors[i]);
         }
       }
-      // convert hex colors to RGBa
-      for (i = 0; i < opts.colors2.length; i++) {
-        if (typeof opts.colors2[i] === 'string') {
-          opts.colors2[i] = this.helpers.hexToRGBa(opts.colors2[i]);
-        }
-      }
 
       pixelData = this.generatePixelData({
         width: width,
         height: height,
         depthMap: depthMap,
-        colors: opts.colors,
-        colors2: opts.colors2
+        colors: opts.colors
       });
 
       switch (element.tagName) {
@@ -171,12 +160,7 @@
           pixelOffset = (y * width * 4) + (x * 4);
           if (same[x] === x) {
             // set random color
-            if (x > width/2){
-            	rgba = opts.colors2[Math.floor(Math.random() * numColors)];
-            }
-            else {
-            	rgba = opts.colors[Math.floor(Math.random() * numColors)];
-            }
+            rgba = opts.colors[Math.floor(Math.random() * numColors)];
             for (i = 0; i < 4; i++) {
               pixels[pixelOffset + i] = rgba[i];
             }
