875557a0e0253876c090e24c5d6069fb7a3b7822
triplelog <robmwilc@gmail.com>
2021-03-31 11:20:58 -0400

    nm

--- static/js/magiceyefastblock.js
+++ static/js/magiceyefastblock.js
@@ -211,7 +211,7 @@
             rgba = colorsFG[x];
             
             var block = true;
-            if (colorsFG[minD+1][0] == colorsFG[minD][0] && colorsFG[minD+1][1] == colorsFG[minD][1]){
+            if (colorsFG[x+1][0] == colorsFG[x][0] && colorsFG[x+1][1] == colorsFG[x][1]){
             	block = false;
             }
             var xi = x;
