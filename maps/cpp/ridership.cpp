f46753747bccdb37a38e730476ec1a34af7da128
triplelog <robmwilc@gmail.com>
2021-05-19 17:27:28 -0400

    nm

--- maps/cpp/ridership.cpp
+++ maps/cpp/ridership.cpp
@@ -116,7 +116,8 @@ void SetLandValue(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 	int i;
 	int row = 0;
 	for (i=0;i<sz;i++){
-		int row1 = jsArr->Get(i)->Int32Value(context).FromJust();
+		v8::Local<v8::Value> jsElement = jsArr->Get(i);
+		int row1 = jsElement->Int32Value(context).FromJust();
 		row += row1;
 		landValue.push_back(row1);
 	}
