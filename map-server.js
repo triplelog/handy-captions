34fdd7205db21c445eb6ffe305325c26361ac461
triplelog <robmwilc@gmail.com>
2021-05-21 13:27:41 -0400

    nm

--- map-server.js
+++ map-server.js
@@ -78,7 +78,13 @@ for (var i=0;i<cityList.length;i++){
 		var x1 = (cityList[ii][0] + 179.14708263665557)/0.0249999999 - 6534/3;
 		var y1 = (cityList[ii][1] - 71.38708322329654)/-0.0249999999 - 2643/3;
 		var d = (x1-x0)*(x1-x0)+(y1-y0)*(y1-y0);
-		if (d < 400 && cityList[i][3] < 10000000){
+		if (d < 900 && cityList[i][3] < 2000000){
+			console.log(cityList[i][2],cityList[ii][2]);
+			cityList.splice(i,1);
+			i--;
+			break;
+		}
+		else if (d < 400 && cityList[i][3] < 5000000){
 			console.log(cityList[i][2],cityList[ii][2]);
 			cityList.splice(i,1);
 			i--;
