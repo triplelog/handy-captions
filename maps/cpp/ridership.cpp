8bf8ffc0c72f3b70b17888db1999c58697918178
triplelog <robmwilc@gmail.com>
2021-05-21 16:10:21 -0400

    nm

--- maps/cpp/ridership.cpp
+++ maps/cpp/ridership.cpp
@@ -338,7 +338,7 @@ void GetStations(const Nan::FunctionCallbackInfo<v8::Value>& info) {
 	int i;
 	std::vector<int> stations;
 	for (i=0;i<sz;i++){
-		szz = jsArr->Get(context,i)->Int32Value(context).FromJust();
+		szz = jsArr->Get(context,i).FromJust();
 		//stations.push_back(jsArr->Get(context,i));
 	}
 	
