cc083b35c5c8f73d317dde4d1d946c8e5ddd9edc
triplelog <robmwilc@gmail.com>
2021-05-25 10:10:57 -0400

    nm

--- blog-server.js
+++ blog-server.js
@@ -65,10 +65,10 @@ function readJson() {
 	  }
 	  var document = JSON.parse(data);
 	  for (var i=0;i<document.pages.length;i++){
-	  	for (var ii=0;ii<Math.min(5,document.pages[i].blocks.length);ii++){//document.pages[i].blocks.length;ii++){
-	  		for (var iii=0;iii<Math.min(5,document.pages[i].blocks[ii].paragraphs.length);iii++){
+	  	for (var ii=0;ii<document.pages[i].blocks.length;ii++){//document.pages[i].blocks.length;ii++){
+	  		for (var iii=0;iii<document.pages[i].blocks[ii].paragraphs.length;iii++){
 	  			for (var iiii=0;iiii<document.pages[i].blocks[ii].paragraphs[iii].words.length;iiii++){
-	  				console.log(document.pages[i].blocks[ii].paragraphs[iii].words[iiii].boundingBox);
+	  				//console.log(document.pages[i].blocks[ii].paragraphs[iii].words[iiii].boundingBox);
 	  				var pd = bbToPath(document.pages[i].blocks[ii].paragraphs[iii].words[iiii].boundingBox.vertices);
 	  				console.log(pd);
 	  			}
