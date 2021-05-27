b60ff898f3e31f94f31276d7d2232e2b54eccd88
triplelog <robmwilc@gmail.com>
2021-05-27 09:39:29 -0400

    nm

--- static/js/blog.js
+++ static/js/blog.js
@@ -227,7 +227,7 @@ function divideWords(strokes) {
 			
 			var hash = hashStrokes(word['strokes']);
 			
-			if (wordsHashed[hash]){
+			if (wordsHashed[hash] && wordsHashed[hash] != {}){
 				console.log("hashed",hash,word);
 				var el = wordsHashed[hash];
 				console.log(el);
