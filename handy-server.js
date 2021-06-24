e974ea892fa8bc6f6566481f84d9dc2ed45c0ebd
triplelog <robmwilc@gmail.com>
2021-06-24 16:32:09 -0400

    nm

--- handy-server.js
+++ handy-server.js
@@ -215,7 +215,8 @@ app.get('/game',
 		else if (req.query && req.query.p){
 			console.log(def);
 			console.log(req.query.p);
-			path = zlib.inflateSync(new Buffer.from(def, 'base64')).toString();
+			var p = req.query.p.toString();
+			path = zlib.inflateSync(new Buffer.from(p, 'base64')).toString();
 			
 		}
 		
