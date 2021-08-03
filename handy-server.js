e548042cb0a335e7d92f9bb5f0c0dc39dd90485a
triplelog <robmwilc@gmail.com>
2021-08-03 17:39:31 -0400

    nm

--- handy-server.js
+++ handy-server.js
@@ -74,6 +74,7 @@ app.get('/magicmaker',
 );
 
 function pathToPoints(path) {
+	console.log(path);
 	path = path.replace(/,/g," ");
 	path = path.replace(/M /g,"M");
 	path = path.replace(/ M/g,"M");
